b3477812213c5c46675f7483d57e603e5105c461
EmiliaSolowicz <esolowicz@edu.cdv.pl>
2023-05-07 15:00:27 +0200

    huhu

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //hihi
+        //huhu
     }
 
     // Update is called once per frame
